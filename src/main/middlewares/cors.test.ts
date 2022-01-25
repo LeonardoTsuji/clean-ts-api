2b3b38d0992357040439faecf41bf71f783cd1dc
LeonardoTsuji <leonardo_tsuji@hotmail.com>
2022-01-25 20:03:56 -0300

    feat: ensure cors is enabled

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,12 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('CORS Middleware', () => {
+  test('Should enable CORS', async () => {
+    const route = '/test_cors'
+    app.get(route, (req, res) => {
+      res.send()
+    })
+    await request(app).get(route).expect('access-control-allow-origin', '*')
+  })
+})
