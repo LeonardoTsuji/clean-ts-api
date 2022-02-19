c80f8993ff9506572ef49817f584a0420682879e
LeonardoTsuji <leonardo_tsuji@hotmail.com>
2022-02-19 17:11:17 -0300

    refactor: improve file names

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -1,7 +1,7 @@
 import { MissingParamError, ServerError } from '../../errors'
 
-import { AccountModel, AddAccount, AddAccountModel, HttpRequest, Validation } from './signup-protocols'
-import { SignUpController } from './signup'
+import { AccountModel, AddAccount, AddAccountModel, HttpRequest, Validation } from './signup-controller-protocols'
+import { SignUpController } from './signup-controller'
 import { badRequest, ok, serverError } from '../../helpers/http/http-helper'
 
 const makeAddAccount = (): AddAccount => {
