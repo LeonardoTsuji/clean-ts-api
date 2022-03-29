95fa4c86da80204d483aeb642378b469b6625eb6
LeonardoTsuji <leonardo_tsuji@hotmail.com>
2022-03-29 20:38:42 -0300

    test: ensure AccountMongoRepository will not find an account if find account by token fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -98,6 +98,7 @@ describe('Account Mongo Repository', () => {
       expect(account.email).toBe('any_email@mail.com')
       expect(account.password).toBe('any_password')
     })
+
     test('Should return an account on loadByToken with role', async () => {
       const sut = makeSut()
       await accountCollection.insertOne({
@@ -114,6 +115,13 @@ describe('Account Mongo Repository', () => {
       expect(account.name).toBe('any_name')
       expect(account.email).toBe('any_email@mail.com')
       expect(account.password).toBe('any_password')
+    })
+
+    test('Should return null if loadByToken fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByToken({ accessToken: 'any_token' })
+
+      expect(account).toBeFalsy()
     })
   })
 })
