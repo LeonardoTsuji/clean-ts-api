661142cf141fb39a824cb77fd0a52d60925a67a8
LeonardoTsuji <leonardo_tsuji@hotmail.com>
2022-05-10 20:34:59 -0300

    refactor: change all paths to use modules

--- src/data/protocols/db/account/load-account-by-token-repository.ts
+++ src/data/protocols/db/account/load-account-by-token-repository.ts
@@ -1,5 +1,5 @@
-import { AccountModel } from '../../../../domain/models/account'
-import { LoadAccountByTokenModel } from '../../../../domain/usecases/load-account-by-token'
+import { AccountModel } from '@/domain/models/account'
+import { LoadAccountByTokenModel } from '@/domain/usecases/load-account-by-token'
 
 export interface LoadAccountByTokenRepository {
   loadByToken (data: LoadAccountByTokenModel): Promise<AccountModel>
--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -1,8 +1,8 @@
-import { LoadAccountByTokenModel } from '../../../domain/usecases/load-account-by-token'
-import { Decrypter } from '../../protocols/cryptography/decrypter'
-import { AccountModel } from '../add-account/db-add-account-protocols'
+import { LoadAccountByTokenModel } from '@/domain/usecases/load-account-by-token'
+import { AccountModel } from '@/domain/models/account'
+import { Decrypter } from '@/data/protocols/cryptography/decrypter'
+import { LoadAccountByTokenRepository } from '@/data/protocols/db/account/load-account-by-token-repository'
 import { DbLoadAccountByToken } from './db-load-account-by-token'
-import { LoadAccountByTokenRepository } from '../../protocols/db/account/load-account-by-token-repository'
 
 const makeFakeAccount = (): AccountModel => ({
   id: 'valid_id',
